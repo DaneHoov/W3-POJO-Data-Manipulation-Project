38806ccc0e034b2e5391cdb39224dd582fd8f321
DoctorDoofen <and.bennett0930@gmail.com>
2024-03-31 20:19:31 -0500

    missing alternate solution to updateKeyName function and still needs a solve for updateIdValues function

--- problems/02-manipulate-fruit-objects.js
+++ problems/02-manipulate-fruit-objects.js
@@ -35,41 +35,53 @@ console.log(addKeyAndValueToOne(fruits, "color", "red", 0));
 objects, and return the resulting array.
 HINT: Can you make a copy of the old key and value, and then delete the original?
 
-console.log(updateKeyName(fruits, "nutritions", "nutrition"));
 // returns fruits array, but every "nutritions" key had changed to "nutrition"
 */
 
 function updateKeyName(array, oldKey, newKey) {
-    // Your code here 
+
+    for (let obj of array) {
+
+        obj[newKey] = obj[oldKey];
+        delete obj[oldKey];
+    }
+    return array;
 }
 
+console.log(updateKeyName(fruits, "nutritions", "nutrition"));
 /* 10. `updateIdValues()` - Change all of the "id" values to six-character
 strings, and return an array of all of the new id values.
 For example: 1 becomes "000001", and 31 becomes "000031"
 
-console.log(updateIdValues(fruits));
 // returns a list of 31 id, in six-character string format:
 // [ '000006', '000035', '000001', '000064', '000033', '000009', '000060',
-    '000068', '000069', '000047', '000072', '000037', '000066', '000026',
-    '000044', '000065', '000067', '000027', '000041', '000002', '000042',
-    '000070', '000004', '000052', '000010', '000071', '000023', '000003',
-    '000005', '000073', '000025' ];
+'000068', '000069', '000047', '000072', '000037', '000066', '000026',
+'000044', '000065', '000067', '000027', '000041', '000002', '000042',
+'000070', '000004', '000052', '000010', '000071', '000023', '000003',
+'000005', '000073', '000025' ];
 */
 
 function updateIdValues(array) {
     // Your code here 
 }
 
+// console.log(updateIdValues(fruits));
 /* 11. `deleteKeysandValues()` - Delete the keyToDelete from the nutritions
 object from every fruit, and return the array.
 
-console.log(deleteKeysAndValues(fruits, "sugar"));
 // returns fruits array, but every "nutritions" key no longer has a "sugar" key
 */
 
 function deleteKeysAndValues(array, keyToDelete) {
-    // Your code here 
+
+    for (let obj of array) {
+        let nutritions = obj.nutritions
+        delete nutritions[keyToDelete];
+    }
+    return array;
 }
+
+console.log(deleteKeysAndValues(fruits, "sugar"));
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
